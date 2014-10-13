392be2ff83f059f8967a884379d46b182f5f2047
Rakshit Shukla <shuklapossible@gmail.com>
2014-10-13 17:56:46 -0400

    Implementet bricks and game is partially working

--- library.js
+++ library.js
@@ -2,42 +2,83 @@ var library = (function() {
 
 		var canvasMinX;
 		var canvasMaxX;
-		var x = 150;
-		var y = 150;
+		var x = 100;
+		var y = 100;
 		var dx = 2;
 		var dy = 4;
-		var WIDTH;
-		var HEIGHT;
+		var canvas_width;
+		var canvas_height;
 		var intervalId = 0;
-		var paddlex;
-		var paddleh;
-		var paddlew;
-		rightDown = false;
-		leftDown = false;
+		var paddlex; // Position of paddle
+		var paddleh; // Paddle height
+		var paddlew; // Paddle width
+		var rightDown = false; 
+		var leftDown = false;
+		var canvasMinX = 0;
+		var canvasMaxX = 0;
+		var bricks; // bricks is a 2D array containing all the bricks 
+		var row_count;
+		var col_count;
+		var brick_width;
+		var brick_height;
+		var padding;
 
 		return {
+				/* 
+					Initialize the canvas.
+				 */
 				init: function() {
 					ctx = $('#canvas')[0].getContext("2d");
-					WIDTH = $('#canvas').width();
-					HEIGHT = $('#canvas').height();
+					canvas_width = $('#canvas').width();
+					canvas_height = $('#canvas').height();
 					intervalId = setInterval(library.draw, 10);
 				},
+				/* 
+					Paddle initialization.
+				*/
 				init_paddle: function() {
-					paddlex = WIDTH / 3;
+					paddlex = canvas_width / 3;
 					paddleh = 10;
 					paddlew = 125;
 				},
+				/* 	
+					Initialize the min/max for mouse gestures
+					Not working.
+				 */
 				init_mouse: function() {
 				 	canvasMinX = $("#canvas").offset().left;
-				 	canvasMaxX = canvasMinX + WIDTH;
+				 	canvasMaxX = canvasMinX + canvas_width;
 				},
+				/*
+					Initialize the bricks
+				 */
+				init_bricks: function() {
+				  	row_count = 5;
+  					col_count = 5;
+					brick_width = (canvas_width/col_count) - 1;
+					brick_height = 15;
+					padding = 1;
+
+					bricks = new Array(row_count);
+					for (i=0; i < row_count; i++) {
+						bricks[i] = new Array(col_count);
+						for (j=0; j < col_count; j++) {
+							bricks[i][j] = 1;
+						}
+					}
+				},
+				/* 
+					Canvas defnition of the ball
+				*/
 				circle: function(x,y,radius) {
 					ctx.beginPath();
 					ctx.arc(x, y, radius, 0, Math.PI*2, true);
 					ctx.closePath();
 					ctx.fill();
 				},
-
+				/* 
+					Canvas definition of bricks and paddle
+				*/
 				rect: function(x,y,w,h) {
 					ctx.beginPath();
 					ctx.rect(x,y,w,h);
@@ -46,9 +87,11 @@ var library = (function() {
 				},
 
 				clear: function() {
-					ctx.clearRect(0, 0, WIDTH, HEIGHT);
+					ctx.clearRect(0, 0, canvas_width, canvas_height);
 				},
-
+				/*
+					Main drawing function
+				*/
 				 draw: function() {
 					library.clear();
 					library.circle(x, y, 10);
@@ -57,26 +100,56 @@ var library = (function() {
 					if (rightDown && paddlex < 300) paddlex += 5;
 					else if (leftDown && paddlex > 0) paddlex -= 5;
 
-					console.log(paddlex)
+					library.rect(paddlex, canvas_height-paddleh, paddlew, paddleh);
 
-					library.rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
-					if (x + dx > WIDTH || x + dx < 0)
+					//Draw  Bricks
+
+					  for (i=0; i < row_count; i++) {
+					    for (j=0; j < col_count; j++) {
+					      if (bricks[i][j] == 1) {
+					        library.rect((j * (brick_width + padding)) + padding, 
+					             (i * (brick_height + padding)) + padding,
+					             brick_width, brick_height);
+					      }
+					    }
+					  }
+
+
+					  //Check if we hit the brick
+					  /*
+						x, y, are spatial locations of the ball
+						dy = -dy makes the ball go in reverse
+					  */
+					  rowheight = brick_height + padding;
+					  colwidth = brick_width + padding;
+					  row = Math.floor(y/rowheight);
+					  col = Math.floor(x/colwidth);
+					  //if so, reverse the ball and mark the brick as broken
+					  if (y < row_count * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
+					    dy = -dy;
+					    bricks[row][col] = 0;
+					  }
+
+					if (x + dx > canvas_width || x + dx < 0)
 						dx = -dx;
 					if(y + dy < 0)
 						dy = -dy
-					else if (y + dy > HEIGHT )
+					else if (y + dy > canvas_height )
 					{
 						if (x > paddlex && x < paddlex + paddlew)
 							dy = -dy;
 						else {
 							clearInterval(intervalId);
-							// alert("boo! You lost!")
+							alert("boo! You lost!")
 						}
 					}
 					x += dx;
 					y += dy;
 				},
 
+				/*
+					Left/Right keys to move the paddle
+				 */
 				onKeyDown: function(e) {
 					if(e.keyCode == 39)
 						{
